8d561e7851fdf13bb2a24395f03188c03f7de16e
Albert <dow.albert@gmail.com>
2020-01-31 14:16:46 +0000

    add gitignore and errors of svfit computation

--- ClassicSVfitTest/bin/ClassicSVFitTest.cpp
+++ ClassicSVfitTest/bin/ClassicSVFitTest.cpp
@@ -116,7 +116,9 @@ int main(int argc, char* argv[]){
   int dm1 = -1;
   int dm2 = -1;
   double svfit_mass;
+  double svfit_mass_err;
   double svfit_transverse_mass;
+  double svfit_transverse_mass_err;
   ic::Candidate *svfit_vector = NULL;
 
   TH1::AddDirectory(kFALSE);
@@ -139,7 +141,9 @@ int main(int argc, char* argv[]){
   otree->Branch("run", &run, "run/i");
   otree->Branch("objects_hash", &objects_hash, "objects_hash/l");
   otree->Branch("svfit_mass", &svfit_mass);
+  otree->Branch("svfit_mass_err", &svfit_mass_err);
   otree->Branch("svfit_transverse_mass", &svfit_transverse_mass);
+  otree->Branch("svfit_transverse_mass_err", &svfit_transverse_mass_err);
   otree->Branch("svfit_vector", &svfit_vector);
 
   for (unsigned i = mini; i < maxi; ++i) {
@@ -204,11 +208,15 @@ int main(int argc, char* argv[]){
 
     if (isValidSolution){
       svfit_mass = static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getMass();
+      svfit_mass_err = static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getMassErr();
       svfit_transverse_mass = static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getTransverseMass();
+      svfit_transverse_mass_err = static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getTransverseMassErr();
      svfit_vector->set_vector((ROOT::Math::PtEtaPhiEVector)ROOT::Math::PtEtaPhiMVector(static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getPt(), static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getEta(), static_cast<DiTauSystemHistogramAdapter*>(svFitAlgo.getHistogramAdapter())->getPhi(), svfit_mass));
     } else {
       svfit_mass = -1;
+      svfit_mass_err = -1;
       svfit_transverse_mass = -1;
+      svfit_transverse_mass_err = -1;
     }
     svfit_vector->set_id(objects_hash);
     std::cout << "Mass: " << svfit_mass << "\tVector Mass: " << svfit_vector->M() << "\tVector pT: " << svfit_vector->pt() << std::endl;
